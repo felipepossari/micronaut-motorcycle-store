68a5358009082267599afd5364a7bfb5837d431d
Felipe Possari <fcpfelipe@hotmail.com>
2021-02-09 07:16:55 -0300

    Adding swagger

--- src/main/kotlin/com/felipepossari/motorcyclestore/Application.kt
+++ src/main/kotlin/com/felipepossari/motorcyclestore/Application.kt
@@ -1,10 +1,27 @@
 package com.felipepossari.motorcyclestore
 
-import io.micronaut.runtime.Micronaut.*
-fun main(args: Array<String>) {
-	build()
-	    .args(*args)
-		.packages("com.felipepossari.motorcyclestore")
-		.start()
+import io.micronaut.runtime.Micronaut.build
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
+
+@OpenAPIDefinition(
+        info = Info(
+                title = "motorcycle-store",
+                version = "1.0.0",
+                description = "Micronaut study project",
+                license = License(name = "MIT")
+        )
+)
+object Application {
+
 }
+
+fun main(args: Array<String>) {
+    build()
+            .args(*args)
+            .packages("com.felipepossari.motorcyclestore")
+            .start()
+}
+
 
