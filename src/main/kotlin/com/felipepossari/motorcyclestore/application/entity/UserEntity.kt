1a571b26d1fbefbe224780e10cffa14c1ca25bd0
Felipe Possari <fcpfelipe@hotmail.com>
2021-02-12 06:48:55 -0300

    Motorcycle entity updated. User entity created

--- src/main/kotlin/com/felipepossari/motorcyclestore/application/entity/UserEntity.kt
+++ src/main/kotlin/com/felipepossari/motorcyclestore/application/entity/UserEntity.kt
@@ -6,14 +6,11 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-data class MotorcycleEntity(
+data class UserEntity(
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
-        val id: Long? = 0,
-        val brand: String = "",
-        val model: String = "",
-        val cubicCylinder: Int = 0,
-        val manufactureYear: Int = 0,
-        val modelYear: Int = 0,
-        val price: Double = 0.0
-)
+        val id: Long?,
+        val username: String = "",
+        val password: String = ""
+) {
+}
