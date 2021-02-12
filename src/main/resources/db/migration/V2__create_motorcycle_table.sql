e86497882e5de86f3e361437e0b5d77b9519ea15
Felipe Possari <fcpfelipe@hotmail.com>
2021-02-12 07:42:05 -0300

    Update column names to fix jpa error

--- src/main/resources/db/migration/V2__create_motorcycle_table.sql
+++ src/main/resources/db/migration/V2__create_motorcycle_table.sql
@@ -2,8 +2,8 @@ CREATE TABLE motorcycle (
     id SERIAL PRIMARY KEY,
     brand VARCHAR(255) NOT NULL,
     model VARCHAR(255) NOT NULL,
-    cubicCylinder INT NOT NULL,
-    manufactureYear INT NOT NULL,
-    modelYear INT NOT NULL,
+    cubic_cylinder INT NOT NULL,
+    manufacture_year INT NOT NULL,
+    model_year INT NOT NULL,
     price NUMERIC(11,2) NOT NULL
 );
