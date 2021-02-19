f66167079298dbc44a9686a23356b8e975ebc6a2
Felipe Possari <fcpfelipe@hotmail.com>
2021-02-19 08:44:05 -0300

    Update create bike use case and add stock amount kafka publisher

--- src/main/kotlin/com/felipepossari/motorcyclestore/application/service/motorcycle/CreateMotorcycleService.kt
+++ src/main/kotlin/com/felipepossari/motorcyclestore/application/service/motorcycle/CreateMotorcycleService.kt
@@ -5,16 +5,19 @@ import com.felipepossari.motorcyclestore.application.builder.buildEntity
 import com.felipepossari.motorcyclestore.application.model.MotorcycleDto
 import com.felipepossari.motorcyclestore.application.port.`in`.motorcycle.CreateMotorcycleUseCase
 import com.felipepossari.motorcyclestore.application.port.out.MotorcycleRepositoryPort
+import com.felipepossari.motorcyclestore.application.port.out.StockAmountPublisherPort
 import javax.inject.Singleton
 
 @Singleton
 class CreateMotorcycleService(
         private val repositoryPort: MotorcycleRepositoryPort,
-        private val requestValidator: MotorcycleRequestValidator) : CreateMotorcycleUseCase {
+        private val requestValidator: MotorcycleRequestValidator,
+        private val stockAmountPublisherPort: StockAmountPublisherPort) : CreateMotorcycleUseCase {
     override fun create(dto: MotorcycleDto): MotorcycleDto {
         requestValidator.validateCreateRequest(dto)
         val entity = buildEntity(dto)
         val newEntity = repositoryPort.save(entity)
+        stockAmountPublisherPort.publishIncreaseStockMessage(dto)
         return buildDto(newEntity)
     }
 }
